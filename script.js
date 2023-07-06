267a4fe81cc45c72a61b37cb00e6bb589f304e95
Yasmin <yasmnnabila@gmail.com>
2023-07-06 12:40:20 +0700

    Init map using Atlantis location in Jkt

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "2df3004fb98b0693",
+    center: { lat: -6.125018940025437, lng: 106.83837820218818 },
+    zoom: 18,
+  });
+}
